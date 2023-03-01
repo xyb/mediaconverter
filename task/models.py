6f546b6be0e671f5ac35e1c64125af50b51d8aaa
Xie Yanbo <xieyanbo@gmail.com>
2023-03-01 17:30:30 +0800

    task models

--- task/models.py
+++ task/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Task(models.Model):
+
+    class Status(models.IntegerChoices):
+        INITED = 1
+        STARTED = 2
+        FINISHED = 3
+
+    path = models.CharField(max_length=1024)
+    status = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    started_at = models.DateTimeField(blank=True)
+    finished_at = models.DateTimeField(blank=True)
+    failed = models.BooleanField(null=True)
+    message = models.CharField(max_length=1000)
