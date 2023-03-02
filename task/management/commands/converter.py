b724cfb1ff83a0d9ff84e8cd01e2e9a46859947c
Xie Yanbo <xieyanbo@gmail.com>
2023-03-02 14:09:09 +0800

    check path for security issues

--- task/management/commands/converter.py
+++ task/management/commands/converter.py
@@ -1,11 +1,13 @@
 from time import sleep
+#import traceback
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 
-from task.models import Task
 from task.converter import convert2mp3
+from task.models import Task
+from task.path import safe_join
 
 
 class Command(BaseCommand):
@@ -15,20 +17,24 @@ class Command(BaseCommand):
         while True:
             tasks = Task.objects.filter(status=Task.Status.INITED)
             for task in tasks:
+                print(f'start convert {task.from_path} to {task.to_path}')
                 task.status = Task.Status.STARTED
                 task.started_at = timezone.now()
                 task.save()
 
-                from_path = settings.DATA_DIR / task.from_path
-                to_path =  settings.DATA_DIR / task.to_path
-
                 failed = False
                 message = ''
                 try:
+                    from_path = safe_join(settings.DATA_DIR, task.from_path)
+                    to_path =  safe_join(settings.DATA_DIR, task.to_path)
                     convert2mp3(from_path, to_path)
+                    print(f'convert {task.to_path} successed.')
                 except Exception as e:
+                    print(f'convert {task.to_path} failed.')
                     failed = True
-                    message = str(e)
+                    #tb = traceback.format_exc()
+                    #message = f'{e}\n{tb}'
+                    message = f'{e}'
 
                 task.status = Task.Status.FINISHED
                 task.finished_at = timezone.now()
---
+++ task/path.py
@@ -0,0 +1,43 @@
+import posixpath
+import os
+
+
+_os_alt_seps = list(
+    sep for sep in [os.sep, os.path.altsep] if sep is not None and sep != "/"
+)
+
+class PathNotSafe(Exception):
+    def __init__(self, path):
+        self.path = path
+
+    def __str__(self):
+        return f'PathNotSafe: {self.path!r}'
+
+
+# based on werkzeug.security.safe_join
+def safe_join(directory, sub_path):
+    """Safely join untrusted path to a base
+    directory to avoid escaping the base directory.
+    """
+    if not directory:
+        # Ensure we end up with ./path if directory="" is given,
+        # otherwise the first untrusted part could become trusted.
+        directory = "."
+
+    parts = [directory]
+
+    filename = sub_path
+    if filename != "":
+        filename = posixpath.normpath(filename)
+
+    if (
+        any(sep in filename for sep in _os_alt_seps)
+        or os.path.isabs(filename)
+        or filename == ".."
+        or filename.startswith("../")
+    ):
+        raise PathNotSafe(sub_path)
+
+    parts.append(filename)
+
+    return posixpath.join(*parts)
